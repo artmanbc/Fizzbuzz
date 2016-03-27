eeec6c950d83f5ea6462166f45f132c77210e8d7
Brenna Artman <artmanbc@dukes.jmu.edu>
2016-03-27 14:06:53 -0400

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,8 +1,29 @@
-exports.count = function(start, end) {
-	var output = start;
-	for (var i = start; i < end; i += 1)  {
-		output = output + " " + (i + 1);
-	}
+var count, fizzer, buzzer, fizzbuzzer;
 
-	return output;
+count = function(start, end) {
+  var output = buzzer(fizzer(fizzbuzzer(start)));
+  for (var i = start; i < end; i += 1)  {
+    output = output + " " + buzzer(fizzer(fizzbuzzer(i + 1)));
+  }
+
+  return output;
 };
+
+fizzer = function(num) {
+  return 0 === num % 3 ? "fizz" : num;
+};
+
+buzzer = function(num) {
+  return 0 === num % 5 ? "buzz" : num;
+};
+
+fizzbuzzer = function(num) {
+  return 0 === num % 15 ? "fizzbuzz" : num;
+}
+
+module.exports = {
+  count: count,
+  fizzer: fizzer,
+  buzzer: buzzer,
+  fizzbuzzer: fizzbuzzer  
+}
